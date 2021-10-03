bb7a17a40c7e94e2311604b5734d2a1cb1752099
Arnav Gosain <arnav@arnavgosain.com>
2021-10-04 02:49:21 +0530

    Fix main

---
+++ src/create-builder.ts
@@ -0,0 +1,3 @@
+import { QueryBuilder } from "./builder";
+
+export const createQueryBuilder = () => new QueryBuilder();
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-import TypedQueryBuilder from "./typed-query";
+import { QueryBuilder } from "./builder";
 
 export type Await<T> = T extends Promise<infer U> ? U : T;
-export type UnknownInstance = TypedQueryBuilder<any, any>;
+export type UnknownInstance = QueryBuilder<any, any>;
